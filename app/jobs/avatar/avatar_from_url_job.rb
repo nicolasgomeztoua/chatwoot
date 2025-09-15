c3a907f485a37f2a4bc5927345039ca12c180056
nicolasgomeztoua <nicolasnourgt@gmail.com>
2025-09-16 01:02:07 +0700

    Revert "Implement reuse of existing avatar blobs in AvatarFromUrlJob for deterministic filenames"
    This reverts commit 295ae575460ab33b449f5166786bef18b1cd156b.

--- app/jobs/avatar/avatar_from_url_job.rb
+++ app/jobs/avatar/avatar_from_url_job.rb
@@ -4,27 +4,6 @@ class Avatar::AvatarFromUrlJob < ApplicationJob
   def perform(avatarable, avatar_url, preferred_filename = nil)
     return unless avatarable.respond_to?(:avatar)
 
-    # Reuse an existing blob when a deterministic filename is supplied (eg: country flags)
-    if preferred_filename.present?
-      existing_blob = ActiveStorage::Blob.where(filename: preferred_filename)
-                                         .where(content_type: ['image/png', 'image/jpeg', 'image/gif'])
-                                         .order(created_at: :asc)
-                                         .first
-      if existing_blob
-        begin
-          # Attach and exit if the object exists on the storage service
-          if existing_blob.service.exist?(existing_blob.key)
-            avatarable.avatar.attach(existing_blob)
-            return
-          end
-        rescue StandardError
-          # If service existence check fails for any reason, fall back to attach
-          avatarable.avatar.attach(existing_blob)
-          return
-        end
-      end
-    end
-
     avatar_file = Down.download(
       avatar_url,
       max_size: 15 * 1024 * 1024
